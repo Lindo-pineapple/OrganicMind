37b9a086b6c3a45b895655a15a966edc05862152
Lindokuhle <lindokuhle.dlomo@pineapple.co.za>
2024-08-08 09:09:19 +0200

    completed the core Today page

--- web/src/app/components/TaskItem.tsx
+++ web/src/app/components/TaskItem.tsx
@@ -5,52 +5,54 @@ import { FaCalendarXmark } from "react-icons/fa6";
 import styles from "@/styles/TaskItem.module.scss";
 
 const TaskItem = (props: { Task: Task }) => {
-  return (
-    <div className={`${styles.TaskContainer}`} onClick={() => {}}>
-      <div className={`${styles.MainTask}`}>
-        <input
-          className={`form-check-input ${styles.TaskCheck}`}
-          type="checkbox"
-          id="flexCheckDefault"
-          checked={props.Task.isDone}
-        />
-        <text className={styles.TaskText}>{props.Task.title}</text>
-        <FaChevronRight className={styles.TaskIcon} />
-      </div>
-      {props.Task.list && props.Task.subtasks && (
-        <div className={`${styles.TaskDetails}`}>
-          <div className={`${styles.TaskList}`}>
-            <div className={styles.ListText}>
-              <FaSquare
-                className={styles.ListIcon}
-                size={18}
-                style={{ color: props.Task.list.listColor }}
-              />
-              {props.Task.list.listName}
-            </div>
+    return (
+      <div className={`${styles.TaskContainer}`} onClick={() => {}}>
+        <div className={`${styles.MainTask}`}>
+          <div className={`${styles.LeftSection}`}>
+            <input
+              className={`form-check-input ${styles.TaskCheck}`}
+              type="checkbox"
+              id="flexCheckDefault"
+              checked={props.Task.isDone}
+            />
+            <text className={styles.TaskText}>{props.Task.title}</text>
           </div>
-          <span className={`${styles.seperator}`}>|</span>
-          <div className={`${styles.SubTasks}`}>
-            <div className={styles.SubTasksText}>
-              <span className={`${styles.badge}`}>
-                {props.Task.subtasks.length}
-              </span>
-              Subtasks
-            </div>
-          </div>
-          {props.Task.dueDate && <span className={`${styles.seperator}`}>|</span>}
-          {props.Task.dueDate && (
-            <div className={`${styles.DueDate}`}>
-              <div className={styles.DateText}>
-                <FaCalendarXmark className={styles.DateIcon} />
-                {props.Task.dueDate}
+          <FaChevronRight className={styles.TaskIcon} />
+        </div>
+        {props.Task.list && props.Task.subtasks && (
+          <div className={`${styles.TaskDetails}`}>
+            <div className={`${styles.TaskList}`}>
+              <div className={styles.ListText}>
+                <FaSquare
+                  className={styles.ListIcon}
+                  size={18}
+                  style={{ color: props.Task.list.listColor }}
+                />
+                {props.Task.list.listName}
               </div>
             </div>
-          )}
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default TaskItem;
+            <span className={`${styles.seperator}`}>|</span>
+            <div className={`${styles.SubTasks}`}>
+              <div className={styles.SubTasksText}>
+                <span className={`${styles.badge}`}>
+                  {props.Task.subtasks.length}
+                </span>
+                Subtasks
+              </div>
+            </div>
+            {props.Task.dueDate && <span className={`${styles.seperator}`}>|</span>}
+            {props.Task.dueDate && (
+              <div className={`${styles.DueDate}`}>
+                <div className={styles.DateText}>
+                  <FaCalendarXmark className={styles.DateIcon} />
+                  {props.Task.dueDate}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    );
+  };
+  
+  export default TaskItem;
