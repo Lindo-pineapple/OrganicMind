a3c55866f04e33db3bd3bbf69482e90885f4614c
Lindokuhle <lindokuhle.dlomo@pineapple.co.za>
2024-08-14 22:18:01 +0200

    mobile sticky wall progress

--- web/src/app/components/StickyWall.tsx
+++ web/src/app/components/StickyWall.tsx
@@ -29,7 +29,7 @@ const StickyWall = (props: { notes: NoteType[]; menuOpen: boolean }) => {
         Sticky Wall
         <FaPlus className={`icon ${styles.addIcon}`} color="grey" size={20} />
       </h1>
-      {!isTabletOrMobile ? (
+      {/* {!isTabletOrMobile ? (
         <Container fluid className={`container ${styles.cardContainer}`}>
           {props.notes.map((note, index) => {
             return (
@@ -98,6 +98,72 @@ const StickyWall = (props: { notes: NoteType[]; menuOpen: boolean }) => {
               </>
             );
           })}
+          <div className={`shadow ${styles.addCardBox}`}>
+            <AiOutlinePlus className={`icon ${styles.plusIcon}`} size={80} />
+          </div>
+        </Container>
+      )} */}
+
+      {!isTabletOrMobile ? (
+        <Container fluid className={`container ${styles.cardContainer}`}>
+          {props.notes.map((note, index) => (
+            <Card
+              key={index} // Add key prop for list rendering
+              className={`shadow ${styles.cardBox}`}
+              style={{ backgroundColor: `${note.color}` }}
+            >
+              <Card.Body className={`${styles.cardBody}`}>
+                <Card.Title>{note.title}</Card.Title>
+                <Card.Text>
+                  <pre style={{ width: "100%", whiteSpace: "pre-wrap" }}>
+                    {note.text}
+                  </pre>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
+          <div className={`shadow ${styles.addCardBox}`}>
+            <AiOutlinePlus className={`icon ${styles.plusIcon}`} size={80} />
+          </div>
+        </Container>
+      ) : (
+        <Container fluid className={`${styles.cardContainer}`}>
+          <div className={`${styles.firstcardContainer}`}>
+            {props.notes.length > 0 && (
+              <Card
+                key={0} // Add key prop for list rendering
+                className={`shadow ${styles.cardBox}`}
+                style={{ backgroundColor: `${props.notes[0].color}` }}
+              >
+                <Card.Body className={`${styles.cardBody}`}>
+                  <Card.Title>{props.notes[0].title}</Card.Title>
+                  <Card.Text>
+                    <pre style={{ width: "100%", whiteSpace: "pre-wrap" }}>
+                      {props.notes[0].text}
+                    </pre>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            )}
+          </div>
+          <div className={`${styles.secondcardContainer}`}>
+            {props.notes.slice(1).map((note, index) => (
+              <Card
+                key={index + 1} // Add key prop for list rendering
+                className={`shadow ${styles.cardBoxSecond}`}
+                style={{ backgroundColor: `${note.color}` }}
+              >
+                <Card.Body className={`${styles.cardBody}`}>
+                  <Card.Title>{note.title}</Card.Title>
+                  <Card.Text>
+                    <pre style={{ width: "100%", whiteSpace: "pre-wrap" }}>
+                      {note.text}
+                    </pre>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
           <div className={`shadow ${styles.addCardBox}`}>
             <AiOutlinePlus className={`icon ${styles.plusIcon}`} size={80} />
           </div>
